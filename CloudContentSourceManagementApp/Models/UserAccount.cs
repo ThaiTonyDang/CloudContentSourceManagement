41d238050dc6015d9409729ed6074a2deeb41a64
Nhu Yen phan <phanyen4311@gmail.com>
2025-07-15 18:14:54 +0700

    [CS] support login and register

---
+++ CloudContentSourceManagementApp/Models/UserAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudContentSourceManagementApp.Models
+{
+    public class UserAccount
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string TenantId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
